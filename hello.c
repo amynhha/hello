b33008f15162c2fe8649a16541e12c9f0c051371
Amy Nguyen <anguyen105@huskers.unl.edu>
2023-08-24 17:27:13 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -6,5 +6,8 @@ int main(int argc, char **argv) {
     printf("I major in Computer Science!\n");
     printf("My name is Amy Nguyen!\n");
 
+    printf("electrical engineering\n");
+    printf("Ethan Aughe!\n");
+
     return 0;
 }
